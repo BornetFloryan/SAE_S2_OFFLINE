1f3296df9077af7280f9cb4381e8b65523585a9e
Floryan Bornet <93878409+BornetFloryan@users.noreply.github.com>
2024-01-22 15:29:11 +0100

    Add files via upload

--- sae_sql.sql
+++ sae_sql.sql
@@ -1,6 +1,18 @@
-DROP TABLE IF EXISTS utilisateur;
+DROP TABLE IF EXISTS vetement, utilisateur, taille, type_vetement;
 
-CREATE TABLE utilisateur (
+CREATE TABLE IF NOT EXISTS taille(
+    id_taille INT NOT NULL AUTO_INCREMENT,
+    libelle_taille VARCHAR(255),
+    PRIMARY KEY(id_taille)
+);
+
+CREATE TABLE IF NOT EXISTS type_vetement(
+    id_type_vetement INT NOT NULL AUTO_INCREMENT,
+    libelle_type_vetement VARCHAR(255),
+    PRIMARY KEY(id_type_vetement)
+);
+
+CREATE TABLE IF NOT EXISTS utilisateur (
     id_utilisateur INT NOT NULL AUTO_INCREMENT,
     login VARCHAR(255),
     email VARCHAR(255),
@@ -10,6 +22,33 @@ CREATE TABLE utilisateur (
     est_actif TINYINT(1),
     PRIMARY KEY(id_utilisateur)
 );
+
+CREATE TABLE IF NOT EXISTS vetement(
+    id_vetement INT NOT NULL AUTO_INCREMENT,
+    nom_vetement VARCHAR(255),
+    prix_vetement FLOAT,
+    matiere VARCHAR(255),
+    description VARCHAR(255),
+    fournisseur VARCHAR(255),
+    marque VARCHAR(255),
+    id_taille INT,
+    id_type_vetement INT,
+    PRIMARY KEY(id_vetement),
+    FOREIGN KEY (id_taille) REFERENCES taille(id_taille),
+    FOREIGN KEY (id_type_vetement) REFERENCES type_vetement(id_type_vetement)
+);
+
+
+INSERT INTO taille(id_taille, libelle_taille) VALUES
+(1, 'S'),
+(2, 'M'),
+(3, 'L'),
+(4, 'XL');
+
+INSERT INTO type_vetement(id_type_vetement, libelle_type_vetement) VALUES
+(1, 'T-shirt'),
+(2, 'Pantalon'),
+(3, 'Chaussures');
 
 INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom,est_actif) VALUES
 (1,'admin','admin@admin.fr',
@@ -21,3 +60,8 @@ INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom,est_actif) 
 (3,'client2','client2@client2.fr',
     'pbkdf2:sha256:600000$3YgdGN0QUT1jjZVN$baa9787abd4decedc328ed56d86939ce816c756ff6d94f4e4191ffc9bf357348',
     'ROLE_client','client2','1');
+
+INSERT INTO vetement(id_vetement, nom_vetement, prix_vetement, matiere, description, fournisseur, marque, id_taille, id_type_vetement) VALUES
+(1, 'T-shirt Nike', 29.99, 'Coton', 'T-shirt confortable et stylé', 'Nike', 'Nike', 1, 1),
+(2, 'Pantalon Levis', 59.99, 'Denim', 'Pantalon en denim de haute qualité', 'Levis', 'Levis', 2, 2),
+(3, 'Chaussures Adidas', 79.99, 'Cuir', 'Chaussures de sport durables', 'Adidas', 'Adidas', 3, 3);
