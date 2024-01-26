40a0869def2ca41c4e442e2d033750175fc175cd
ImmerAlex <105159979+ImmerAlex@users.noreply.github.com>
2024-01-26 08:24:05 +0100

    Add files via upload

--- controllers/fixtures_load.py
+++ controllers/fixtures_load.py
@@ -5,86 +5,23 @@ import datetime
 from decimal import *
 from connexion_db import get_db
 
-fixtures_load = Blueprint('fixtures_load', __name__,
-                        template_folder='templates')
+fixtures_load = Blueprint('fixtures_load', __name__, template_folder='templates')
+
 
 @fixtures_load.route('/base/init')
 def fct_fixtures_load():
-    mycursor = get_db().cursor()
-    sql='''DROP TABLE IF EXISTS   '''
-
-    mycursor.execute(sql)
-    sql='''
-    CREATE TABLE utilisateur(
-  
-    )  DEFAULT CHARSET utf8;  
-    '''
-    mycursor.execute(sql)
-    sql=''' 
-    INSERT INTO utilisateur
-    '''
-    mycursor.execute(sql)
-
-    sql=''' 
-    CREATE TABLE type_article(
-    
-    )  DEFAULT CHARSET utf8;  
-    '''
-    mycursor.execute(sql)
-    sql=''' 
-INSERT INTO type_article
-    '''
-    mycursor.execute(sql)
+     cursor = get_db().cursor()
+     
+     with open('sae_sql.sql', 'r') as f:
+          sql = f.read()
+          sqlCommands = sql.replace('\n', '').split(';')
+          for command in sqlCommands:
+               try:
+                    if command.strip() != '':
+                         cursor.execute(command)
+                         get_db().commit()
+               except IOError:
+                    print(f"Command skipped: {command}")
 
 
-    sql=''' 
-    CREATE TABLE etat (
-    )  DEFAULT CHARSET=utf8;  
-    '''
-    mycursor.execute(sql)
-    sql = ''' 
-INSERT INTO etat
-     '''
-    mycursor.execute(sql)
-
-    sql = ''' 
-    CREATE TABLE article (
-    )  DEFAULT CHARSET=utf8;  
-     '''
-    mycursor.execute(sql)
-    sql = ''' 
-    INSERT INTO article (
-
-         '''
-    mycursor.execute(sql)
-
-    sql = ''' 
-    CREATE TABLE commande (
-    ) DEFAULT CHARSET=utf8;  
-     '''
-    mycursor.execute(sql)
-    sql = ''' 
-    INSERT INTO commande 
-                 '''
-    mycursor.execute(sql)
-
-    sql = ''' 
-    CREATE TABLE ligne_commande(
-    );
-         '''
-    mycursor.execute(sql)
-    sql = ''' 
-    INSERT INTO ligne_commande 
-         '''
-    mycursor.execute(sql)
-
-
-    sql = ''' 
-    CREATE TABLE ligne_panier (
-    );  
-         '''
-    mycursor.execute(sql)
-
-
-    get_db().commit()
-    return redirect('/')
+     return redirect('/')
