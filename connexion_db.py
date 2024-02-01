1e97641fe95c8e899f7c2870fe86a315136c5c31
Floryan Bornet <93878409+BornetFloryan@users.noreply.github.com>
2024-02-01 15:38:54 +0100

    Add files via upload

--- connexion_db.py
+++ connexion_db.py
@@ -2,17 +2,18 @@ from flask import Flask, request, render_template, redirect, url_for, abort, fla
 
 import pymysql.cursors
 
+import os                                 # à ajouter
+from dotenv import load_dotenv            # à ajouter
+load_dotenv()                             # à ajouter
+
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        #
-        db = g._database = pymysql.connect(
-            host="vetementsae.mysql.pythonanywhere-services.com",
-            # host="serveurmysql",
-            user="vetementsae",
-            password="alexleplusbeau",
-            database="vetementsae$BDD_sae",
+    if 'db' not in g:
+        g.db =  pymysql.connect(
+            host=os.environ.get("HOST"),                # à modifier
+            user=os.environ.get("LOGIN"),               # à modifier
+            password=os.environ.get("PASSWORD"),        # à modifier
+            database=os.environ.get("DATABASE"),        # à modifier
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
-    return db
+    return g.db
