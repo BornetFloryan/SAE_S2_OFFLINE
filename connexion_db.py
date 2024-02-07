42423143db8e766ebb1775eeed1cec4ddb03e7da
LightDemon52 <floryan.bornet@gmail.com>
2024-02-07 11:24:50 +0100

    d

--- connexion_db.py
+++ connexion_db.py
@@ -1,18 +1,16 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
-
 import pymysql.cursors
+import os
+from dotenv import load_dotenv
 
-import os                                 # à ajouter
-from dotenv import load_dotenv            # à ajouter
-load_dotenv('./.env')                             # à ajouter
-
+load_dotenv('./.env')
 def get_db():
     if 'db' not in g:
         g.db =  pymysql.connect(
-            host=os.environ.get("HOST"),                # à modifier
-            user=os.environ.get("LOGIN"),               # à modifier
-            password=os.environ.get("PASSWORD"),        # à modifier
-            database=os.environ.get("DATABASE"),        # à modifier
+            host=os.environ.get("HOST"),
+            user=os.environ.get("LOGIN"),
+            password=os.environ.get("PASSWORD"),
+            database=os.environ.get("DATABASE"),
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
