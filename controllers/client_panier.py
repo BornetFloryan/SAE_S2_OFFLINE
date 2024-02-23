c816c8a78b1aa99827bea98dc49a15c970862cba
ImmerAlex <105159979+ImmerAlex@users.noreply.github.com>
2024-02-23 16:11:46 +0100

    Add files via upload

--- controllers/client_panier.py
+++ controllers/client_panier.py
@@ -47,7 +47,7 @@ def client_panier_add():
         mycursor.execute(sql, tuple_insert)
         dec_stock()
 
-#ajout dans le panier d'une déclinaison d'un article (si 1 declinaison : immédiat sinon => vu pour faire un choix
+    # ajout dans le panier d'une déclinaison d'un article (si 1 declinaison : immédiat sinon => vu pour faire un choix
     sql = '''  SELECT *
     FROM taille
     LEFT JOIN stock_vetement ON stock_vetement.id_taille = taille.id_taille
@@ -150,22 +150,117 @@ def client_panier_delete_line():
     return redirect('/client/article/show')
 
 
+
+
+
+
 @client_panier.route('/client/panier/filtre', methods=['POST'])
-def client_panier_filtre():
+def client_panier_filtre():    
+    
     filter_word = request.form.get('filter_word', None)
     filter_prix_min = request.form.get('filter_prix_min', None)
     filter_prix_max = request.form.get('filter_prix_max', None)
-    filter_types = request.form.getlist('filter_types', None)
-    # test des variables puis
-    # mise en session des variables
-    return redirect('/client/article/show')
+    filter_types = request.form.getlist('filter_types')
+    tup_filtre = ()
+
+    sql = ''' SELECT vetement.id_vetement as id_article, 
+                nom_vetement as nom, 
+                prix_vetement as prix,  
+                image as image, 
+                id_type_vetement as id_type,
+                stock_vetement.stock as stock 
+              FROM vetement
+              LEFT JOIN stock_vetement on vetement.id_vetement = stock_vetement.id_vetement
+              WHERE 1=1 '''
+
+    if filter_word is not None and filter_word != '':
+        sql += "AND nom_vetement LIKE %s "
+        tup_filtre += ('%' + filter_word + '%',)
+
+    if (filter_prix_max is not None and filter_prix_max != '') and (filter_prix_min is not None and filter_prix_min != ''):
+        if filter_prix_max < filter_prix_min:
+            flash("Le prix maximum ne peut pas être inférieur au prix minimum", "alert-danger")
+            return redirect('/client/article/show')
+        if filter_prix_max.isalpha() or filter_prix_min.isalpha():
+            flash("Les prix doivent être des nombres", "alert-danger")
+            return redirect('/client/article/show')
+        sql += "AND prix_vetement BETWEEN %s AND %s "
+        tup_filtre += (filter_prix_min, filter_prix_max)
+    elif (filter_prix_max is not None and filter_prix_max != ''):
+        if filter_prix_max.isalpha():
+            flash("Le prix maximum doit être un nombre", "alert-danger")
+            return redirect('/client/article/show')
+        sql += "AND prix_vetement <= %s "
+        tup_filtre += (filter_prix_max,)
+    elif (filter_prix_min is not None and filter_prix_min != ''):
+        if filter_prix_min.isalpha():
+            flash("Le prix minimum doit être un nombre", "alert-danger")
+            return redirect('/client/article/show')
+        sql += "AND prix_vetement >= %s "
+        tup_filtre += (filter_prix_min,)
+
+    if filter_types is not None and len(filter_types) > 0:
+        if len(filter_types) == 1:
+            sql += "AND id_type_vetement = %s "
+            tup_filtre += (filter_types[0],)
+        else:
+            for i in range(len(filter_types)):
+                if i == 0:
+                    sql += "AND (id_type_vetement = %s "
+                elif i == len(filter_types) - 1:
+                    sql += "OR id_type_vetement = %s) "
+                else:
+                    sql += "OR id_type_vetement = %s "
+                tup_filtre += (filter_types[i],)
+
+    mycursor = get_db().cursor()
+    mycursor.execute(sql, tup_filtre)
+    articles = mycursor.fetchall()
+
+    session['filter_word'] = filter_word
+    session['filter_prix_min'] = filter_prix_min
+    session['filter_prix_max'] = filter_prix_max
+    session['filter_types'] = filter_types
+    
+    
+    id_client = session['id_user']
+    sql = '''SELECT vetement_id as id_article,
+            nom_vetement as nom,
+            quantite,
+           ROUND(prix_vetement, 2) as prix,
+           stock
+           FROM ligne_panier
+           LEFT JOIN vetement on vetement.id_vetement = ligne_panier.vetement_id
+              LEFT JOIN stock_vetement on vetement.id_vetement = stock_vetement.id_vetement
+           WHERE utilisateur_id = %s
+           GROUP BY nom_vetement, quantite, prix_vetement, vetement_id, stock
+                      '''
+    mycursor.execute(sql, (id_client,))
+    articles_panier = mycursor.fetchall()
+    
+
+    sql = '''SELECT id_type_vetement  AS id_type_article
+             ,libelle_type_vetement AS libelle
+             FROM type_vetement
+             ORDER BY  libelle_type_vetement
+                '''
+    mycursor.execute(sql)
+    types_article = mycursor.fetchall()
+
+    return render_template('client/boutique/panier_article.html', articles=articles, articles_panier=articles_panier, items_filtre=types_article)
 
 
 @client_panier.route('/client/panier/filtre/suppr', methods=['POST'])
 def client_panier_filtre_suppr():
-    # suppression  des variables en session
-    print("suppr filtre")
+    session.pop('filter_word', None)
+    session.pop('filter_prix_min', None)
+    session.pop('filter_prix_max', None)
+    session.pop('filter_types', None)
     return redirect('/client/article/show')
+
+
+
+
 
 def dec_stock():
     id_vetement = request.form.get('id_article')
